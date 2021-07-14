44187338e584461daf72962a4240e9c6fd9e8c6f
MyqtAsyrof <mshohibulasrof@gmail.com>
2021-07-14 19:52:28 +0700

    tambah data
    mengubah sql dan tombol reset

--- dashboard/tambah.php
+++ dashboard/tambah.php
@@ -1,6 +1,6 @@
 <?php include('config.php'); ?>
 
-<center><strong><span style="font-size: 16px;">Create New Conference</span></strong></center>
+<center><strong><span style="font-size: 30px;">Create New Conference</span></strong></center>
 <hr>
 <?php
 if (isset($_POST['submit'])) {
@@ -11,13 +11,12 @@ if (isset($_POST['submit'])) {
 		$desc 		= $_POST['desc'];
 		$schedule   = $_POST['schedule'];
 		$location   = $_POST['location'];
-		$sql = "INSERT INTO users(name,desc,schedule,location)
-            VALUES('$name', '$desc', '$schedule', '$location')";
+		$sql = "INSERT INTO confer(name, desc, schedule, location) VALUES('$name', '$desc', '$schedule', '$location')";
 
 		if ($koneksi->query($sql) === TRUE) {
-			echo "<div class='alert alert-success'>Successfully added new data</div>";
+			echo "<div class='alert alert-success'>Successfully created a new conference</div>";
 		} else {
-			echo "<div class='alert alert-danger'>Error: There was an error while adding new data</div>";
+			echo "<div class='alert alert-danger'>Error: There was an error while creating a new conference</div>";
 		}
 	}
 }
@@ -33,7 +32,7 @@ if (isset($_POST['submit'])) {
 	<div class="item form-group">
 		<label class="col-form-label col-md-3 col-sm-3 label-align">Description</label>
 		<div class="col-md-6 col-sm-6">
-			<textarea name="desc" class="form-control" required></textarea>
+			<textarea style="height: 150px;" name="desc" class="form-control" required></textarea>
 		</div>
 	</div>
 	<div class="item form-group">
@@ -61,7 +60,7 @@ if (isset($_POST['submit'])) {
 	<div class="item form-group">
 		<div class="col-md-6 col-sm-6 offset-md-3">
 			<input type="submit" name="submit" class="btn btn-primary" value="SAVE">
-			<input type="submit" name="submit" class="btn btn-outline-secondary" value="RESET">
+			<input type="reset" name="reset" class="btn btn-outline-secondary" value="RESET">
 		</div>
 	</div>
 </form>
